1ef02796138310f08fed7abbf657bbda2850eae7
Alveena Ali <alveenaali1101@gmail.com>
2024-06-05 22:39:58 +0500

    push my atm to github

---
+++ index.ts
@@ -0,0 +1,74 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+interface ansType {
+    userId: string,
+    userPin: number,
+    accountType: string,
+    amount: number,
+}
+
+const answers: ansType = await inquirer.prompt([
+    {
+        type: "input",
+        name: "userId",
+        message: "Kindly enter your Id:"
+    },
+
+    {
+        type: "number",
+        name: "userPin",
+        message: "Kindly enter your Pin:"
+    },
+
+    {
+        type: "list",
+        name: "accountType",
+        choices: ["Current" , "Saving"],
+        message: "Please Select Your Account Type",
+    },
+
+    {
+        type: "list",
+        name: "transactionType",
+        choices: ["Fast Cash" , "Withdrawl"],
+        message: "Select Your Transaction",
+        when(answers) {
+            return answers.accountType == "Current";
+        }
+    }, 
+    
+    {
+        type: "number",
+        name: "amount",
+        choices: [1000 , 2000 , 10000 , 20000],
+        message: "Select Your Amount",
+        when(answers) {
+            return answers.transactionType ==  "Fast Cash";
+        }
+    },
+
+    {
+        type: "number",
+        name: "amount",
+        message: "Enter the Withdrawl Amount:",
+        when(answers) {
+            return answers.transactionType == "Withdrawl";
+        }
+    },
+
+]);
+
+if (answers.userId && answers.userPin) {
+    const balance = Math.floor(Math.random() * 10000000);
+    console.log("Current Balance");
+    
+    const enteredAmount = answers.amount;
+    if (balance <= enteredAmount) {
+        console.log("Insuficient Balance");
+    } else {
+        const remaining = balance - enteredAmount;
+        console.log("Your remaining balance is" , remaining);
+    }
+}
